14455b61e963ab9d23c728ccb9135277fb6a3716
Brian Shaginaw <bshaginaw120@gmail.com>
2012-11-16 23:46:42 -0500

    Holy jeez jk this all works :D

--- client.c
+++ client.c
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 
     char buffer[256];
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
     }
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -36,8 +36,8 @@ int main(int argc, char *argv[])
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+        (char *)&serv_addr.sin_addr.s_addr,
+        server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
@@ -46,12 +46,29 @@ int main(int argc, char *argv[])
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+        error("ERROR writing to socket");
+    FILE *file = fopen("cat.jpg", "w");
+    if (file == NULL)
+    {
+        printf("Couldn't open dat file\n");
+        close(sockfd);
+        return;
+    }
+    int i;
+    while(1)
+    {
+        bzero(buffer,256);
+        n = read(sockfd, buffer, 256);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        // fprintf(file, "%s", buffer);
+        for (i = 0; i < n; i++)
+            fputc(buffer[i], file);
+        if (n < 256)
+            break;
+        printf("%s\n",buffer);
+    }
+    fclose(file);
     close(sockfd);
     return 0;
 }
